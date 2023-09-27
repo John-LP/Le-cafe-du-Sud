f5b2aab7e1e25cd7df1df7af670f8b9d48097ca5
John-LP <jonathanleplat@hotmail.fr>
2023-09-27 17:20:55 +0200

    fin journée 1, JS manque tableau + LS

--- script.js
+++ script.js
@@ -2,48 +2,119 @@ let ajouter = document.querySelector(".ajouter");
 let soft = document.querySelector(".soft");
 let alcool = document.querySelector(".alcool");
 let modal = document.querySelector(".modal-body");
-let produit =  document.querySelector(".nom");
+let produit = document.querySelector(".nom");
 let type = document.querySelector(".type");
-let degres =  document.querySelector(".degres");
+let degres = document.querySelector(".degres");
 let achat = document.querySelector(".achat");
-let vente =  document.querySelector(".vente");
-let displayTVA = document.querySelector(".displayTVA");
+let vente = document.querySelector(".vente");
 let stock = document.querySelector(".ajoutStock");
 let addButton = document.querySelector(".boutonValider");
 let LSsoft = [];
 let LSalcool = [];
 
-addButton.addEventListener("click", function() {
-    if (type.value == "soft") {
-        taxe = 0.055;
-        let prixAchatHt = achat.value;
-        let prixVenteHt = vente.value;
-        let margeHt = parseFloat(prixVenteHt) - parseFloat(prixAchatHt);
-        let prixTtc = parseFloat(parseFloat(prixVenteHt) + parseFloat(prixVenteHt) * parseFloat(taxe)).toFixed(2);
-        soft.innerHTML = `<p> 
-        ${produit.value} : ${stock.value}. 
-        Prix de vente TTC : ${prixTtc}€
+// if (LSsoftString) {
+//     LSsoft = LSsoftString;
+//     renderStock(LSsoft);
+//   } else {
+//     LSsoft = [];
+//   }
+//     if (LSalcoolString) {
+//     LSalcool = LSalcoolString;
+//     renderStock(LSalcool);
+//   } else {
+//     LSalcool = [];
+//   }
+
+addButton.addEventListener("click", function () {
+  if (type.value == "soft") {
+    let taxe = 0.055;
+    let prixAchatHt = achat.value;
+    let prixVenteHt = vente.value;
+    let margeHt = parseFloat(prixVenteHt) - parseFloat(prixAchatHt);
+    let prixTtc = parseFloat(
+      parseFloat(prixVenteHt) + parseFloat(prixVenteHt) * parseFloat(taxe)
+    ).toFixed(2);
+    let liste = document.createElement("div");
+    liste.classList.add("liste");
+    liste.innerHTML = `<p class="nomProduit">${produit.value} (${stock.value})</p>
+            <select class="details">
+                <option>TTC : ${prixTtc}€</option>
+                <option>Achat HT : ${prixAchatHt}€</option>
+                <option>Vente HT : ${prixVenteHt}€</option>
+                <option>Marge HT : ${margeHt}€</option>
+            </select>`;
+    let suppr = document.createElement("button");
+    suppr.innerText = "X";
+    suppr.style.color = "#A52A2A";
+    suppr.addEventListener("click", function () {
+      liste.remove();
+    });
+    liste.appendChild(suppr);
+    soft.appendChild(liste);
+    LSsoft.push(`
+        Produit : ${produit.value}
+        Stock : ${stock.value}
+        Achat HT : ${prixAchatHt}€
+        Vente HT : ${prixVenteHt}€
         Marge HT : ${margeHt}€
-        </p>`
-        displayTVA.innerHTML = `<p> TVA = 5.5%</p>`
+        TTC : ${prixTtc}€`);
+    let LSsoftString = JSON.stringify(LSsoft);
+    localStorage.setItem("LSsoft", LSsoftString);
+  } else {
+    taxe = 0.2;
+    let prixAchatHt = achat.value;
+    let prixVenteHt = vente.value;
+    let margeHt = parseFloat(prixVenteHt) - parseFloat(prixAchatHt);
+    let prixTtc = parseFloat(
+      parseFloat(prixVenteHt) + parseFloat(prixVenteHt) * parseFloat(taxe)
+    ).toFixed(2);
+    let liste = document.createElement("div");
+    liste.classList.add("liste");
+    liste.innerHTML = `<p class="nomProduit">${produit.value} (${stock.value})</p>
+            <select class="details">
+                <option>TTC : ${prixTtc}€</option>
+                <option>Achat HT : ${prixAchatHt}€</option>
+                <option>Vente HT : ${prixVenteHt}€</option>
+                <option>Marge HT : ${margeHt}€</option>
+                <option>Degré d'alcool : ${degres.value}%</option>
+            </select>`;
+    let suppr = document.createElement("button");
+    suppr.innerText = "X";
+    suppr.style.color = "#A52A2A";
+    suppr.addEventListener("click", function () {
+      liste.remove();
+    });
+    liste.appendChild(suppr);
+    alcool.appendChild(liste);
+
+    LSalcool.push(`
+        Produit : ${produit.value}
+        Stock : ${stock.value}
+        Prix d'achat HT : ${prixAchatHt}€
+        Prix de vente HT : ${prixVenteHt}€
+        Marge HT : ${margeHt}€
+        TTC : ${prixTtc}€
+        Degré d'alcool : ${degres}%`);
+  }
+  let nomProduits = document.querySelectorAll(".nomProduit");
+  nomProduits.forEach(function (nomProduit) {
+    if (stock.value <= 5) {
+      nomProduit.style.color = "#A52A2A";
     } else {
-        taxe = 0.2;
-        let prixAchatHt = achat.value;
-        let prixVenteHt = vente.value;
-        let margeHt = parseFloat(prixVenteHt) - parseFloat(prixAchatHt);
-        let prixTtc = parseFloat(prixVenteHt) + parseFloat(prixVenteHt) * parseFloat(taxe);
-        alcool.innerHTML = `<p> 
-        ${produit.value} : ${stock.value}. <br/>
-        Prix de vente TTC : ${prixTtc}€ <br/>
-        Marge HT : ${margeHt}€ <br/>
-        </p>`
-        displayTVA.innerHTML = `<p> TVA = 20%</p>`
+      nomProduit.style.color = "green";
     }
-})
+  });
+});
 
-
-// let produit = {
-//     achat: data.get("achat"),
-//     vente: data.get("vente"),
-//     taxe: data.get("taxe"),
-// }
+// function renderStock() {
+//     soft.innerHTML = "";
+//     LSsoft.forEach(function (produit.value,stock.value,prixAchatHt,prixVenteHt,margeHt,prixTtc) {
+//         alcool.innerHTML += `<div class="liste"><p class="nomProduit">${produit.value} (${stock.value})</p>
+//         <select class="details">
+//         <option>TTC : ${prixTtc}€</option>
+//         <option>Achat HT : ${prixAchatHt}€</option>
+//         <option>Vente HT : ${prixVenteHt}€</option>
+//         <option>Marge HT : ${margeHt}€</option>
+//         <option>Degré d'alcool : ${degres.value}%</option>
+//         </select></div>`
+//     });}
