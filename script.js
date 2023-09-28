845622208b4b59f2094ae1e5d534e579ecb2d40b
John-LP <jonathanleplat@hotmail.fr>
2023-09-28 18:23:35 +0200

    final push

--- script.js
+++ script.js
@@ -11,6 +11,10 @@ document.addEventListener("DOMContentLoaded", function () {
   let LSsoft = [];
   let LSalcool = [];
 
+  function generateUniqueId() {
+    return "_" + Math.random().toString(36).substr(2, 9);
+  }
+
   const ajouterModal = document.querySelector("#ajouterModal");
   if (ajouterModal) {
     function ouvrirModal() {
@@ -26,14 +30,13 @@ document.addEventListener("DOMContentLoaded", function () {
     LSsoft = JSON.parse(LSsoftString);
     renderStock(LSsoft, soft);
   }
-
   let LSalcoolString = localStorage.getItem("LSalcool");
   if (LSalcoolString) {
     LSalcool = JSON.parse(LSalcoolString);
     renderStock(LSalcool, alcool);
   }
-
   addButton.addEventListener("click", function () {
+    const uniqueId = generateUniqueId();
     if (type.value == "soft") {
       let taxe = 0.055;
       let prixAchatHt = achat.value;
@@ -42,8 +45,8 @@ document.addEventListener("DOMContentLoaded", function () {
       let prixTtc = parseFloat(
         parseFloat(prixVenteHt) + parseFloat(prixVenteHt) * parseFloat(taxe)
       ).toFixed(2);
-
       const softData = {
+        id: uniqueId,
         produit: produit.value,
         stock: stock.value,
         prixAchatHt: prixAchatHt,
@@ -51,11 +54,9 @@ document.addEventListener("DOMContentLoaded", function () {
         margeHt: margeHt,
         prixTtc: prixTtc,
       };
-
       LSsoft.push(softData);
       let LSsoftString = JSON.stringify(LSsoft);
       localStorage.setItem("LSsoft", LSsoftString);
-
       renderStock([softData], soft);
     } else {
       let taxe = 0.2;
@@ -65,8 +66,8 @@ document.addEventListener("DOMContentLoaded", function () {
       let prixTtc = parseFloat(
         parseFloat(prixVenteHt) + parseFloat(prixVenteHt) * parseFloat(taxe)
       ).toFixed(2);
-
       const alcoolData = {
+        id: uniqueId,
         produit: produit.value,
         stock: stock.value,
         prixAchatHt: prixAchatHt,
@@ -75,33 +76,33 @@ document.addEventListener("DOMContentLoaded", function () {
         prixTtc: prixTtc,
         degres: degres.value,
       };
-
       LSalcool.push(alcoolData);
       let LSalcoolString = JSON.stringify(LSalcool);
       localStorage.setItem("LSalcool", LSalcoolString);
-
       renderStock([alcoolData], alcool);
     }
-
-    let nomProduits = document.querySelectorAll(".nomProduit");
-    nomProduits.forEach(function (nomProduit) {
-      if (stock.value <= 5) {
-        nomProduit.style.color = "#A52A2A";
-      } else {
-        nomProduit.style.color = "green";
-      }
-    });
   });
-
   function renderStock(stockData, container) {
     stockData.forEach(function (item) {
       let liste = document.createElement("div");
       liste.classList.add("liste");
-      liste.innerHTML = `<p class="nomProduit">${
-        item.produit
-      } <input class="modifyStock" type="number" value="${
-        item.stock
-      }"></input></p>
+      liste.style.fontFamily = "'Croissant One', cursive";
+      let inputNumber = document.createElement("input");
+      inputNumber.classList.add("modifyStock");
+      inputNumber.type = "number";
+      inputNumber.value = item.stock;
+      inputNumber.addEventListener("change", function () {
+        let value = parseFloat(inputNumber.value);
+        if (value <= 5) {
+          inputNumber.style.color = "#A52A2A";
+        } else {
+          inputNumber.style.color = "green";
+        }
+        if (value < 0) {
+          inputNumber.value = 0;
+        }
+      });
+      liste.innerHTML = `<p class="nomProduit">${item.produit}</p>
             <select class="details">
                 <option>TTC : ${item.prixTtc}€</option>
                 <option>Achat HT : ${item.prixAchatHt}€</option>
@@ -111,19 +112,59 @@ document.addEventListener("DOMContentLoaded", function () {
                   item.degres
                     ? `<option>Degré d'alcool : ${item.degres}%</option>`
                     : ""
-                }
-            </select>`;
-      let suppr = document.createElement("button");
-      suppr.innerText = "X";
-      suppr.style.color = "#A52A2A";
-      suppr.addEventListener("click", function () {
-        liste.remove();
-      });
-      let updateButton = document.createElement("button");
-      updateButton.innerText = "Modifier";
-      updateButton.addEventListener("click", function () {});
-      liste.appendChild(updateButton);
-      liste.appendChild(suppr);
+                }</select>`;
+      if ((type.value = "alcool")) {
+        liste.appendChild(inputNumber);
+        let updateButton = document.createElement("button");
+        updateButton.classList.add("updateButton");
+        updateButton.innerText = "Modifier";
+        updateButton.addEventListener("click", function () {});
+        liste.setAttribute("data-id", item.id);
+        let suppr = document.createElement("button");
+        suppr.innerText = "X";
+        suppr.style.color = "#A52A2A";
+        suppr.setAttribute("data-id", item.id);
+        suppr.addEventListener("click", function () {
+          let itemId = suppr.dataset.id;
+          if (type.value === "soft") {
+            LSsoft = LSsoft.filter((softItem) => softItem.id !== itemId);
+            localStorage.setItem("LSsoft", JSON.stringify(LSsoft));
+          } else {
+            LSalcool = LSalcool.filter(
+              (alcoolItem) => alcoolItem.id !== itemId
+            );
+            localStorage.setItem("LSalcool", JSON.stringify(LSalcool));
+          }
+          liste.remove();
+        });
+        liste.appendChild(updateButton);
+        liste.appendChild(suppr);
+      } else {
+        liste.appendChild(inputNumber);
+        let updateButton = document.createElement("button");
+        updateButton.classList.add("updateButton");
+        updateButton.innerText = "Modifier";
+        updateButton.addEventListener("click", function () {});
+        liste.setAttribute("data-id", item.id);
+        let suppr = document.createElement("button");
+        suppr.innerText = "X";
+        suppr.style.color = "#A52A2A";
+        suppr.setAttribute("data-id", item.id);
+        suppr.addEventListener("click", function () {
+          let itemId = suppr.dataset.id;
+          if (type.value === "soft") {
+            LSsoft = LSsoft.filter((softItem) => softItem.id !== itemId);
+            localStorage.setItem("LSsoft", JSON.stringify(LSsoft));
+          } else {
+            LSalcool = LSalcool.filter(
+              (alcoolItem) => alcoolItem.id !== itemId
+            );
+            localStorage.setItem("LSalcool", JSON.stringify(LSalcool));
+          }
+          liste.remove();
+        });
+        liste.appendChild(suppr);
+      }
       container.appendChild(liste);
     });
   }
