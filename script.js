6e1c018adadd795275913c48084e4bcb6b45b6f8
John-LP <jonathanleplat@hotmail.fr>
2023-09-28 14:58:29 +0200

    MàJ du modal, compression et clean du code JS, mise en place de l'afficahce du LS au lancement de la page

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const ajouterModal = document.querySelector("#ajouterModal");
   if (ajouterModal) {
-    ajouterModal.addEventListener("show.bs.modal", (event) => {});
     function ouvrirModal() {
       const modal = new bootstrap.Modal(ajouterModal);
       modal.show();
@@ -25,13 +24,13 @@ document.addEventListener("DOMContentLoaded", function () {
   let LSsoftString = localStorage.getItem("LSsoft");
   if (LSsoftString) {
     LSsoft = JSON.parse(LSsoftString);
-    renderStock(LSsoft);
+    renderStock(LSsoft, soft);
   }
 
   let LSalcoolString = localStorage.getItem("LSalcool");
   if (LSalcoolString) {
     LSalcool = JSON.parse(LSalcoolString);
-    renderStock(LSalcool);
+    renderStock(LSalcool, alcool);
   }
 
   addButton.addEventListener("click", function () {
@@ -43,77 +42,47 @@ document.addEventListener("DOMContentLoaded", function () {
       let prixTtc = parseFloat(
         parseFloat(prixVenteHt) + parseFloat(prixVenteHt) * parseFloat(taxe)
       ).toFixed(2);
-      let liste = document.createElement("div");
-      liste.classList.add("liste");
-      liste.innerHTML = `<p class="nomProduit">${produit.value} <input class="modifyStock" type="number" value="${stock.value}"></input></p>
-            <select class="details">
-                <option>TTC : ${prixTtc}€</option>
-                <option>Achat HT : ${prixAchatHt}€</option>
-                <option>Vente HT : ${prixVenteHt}€</option>
-                <option>Marge HT : ${margeHt}€</option>
-            </select>`;
-      let suppr = document.createElement("button");
-      suppr.innerText = "X";
-      suppr.style.color = "#A52A2A";
-      suppr.addEventListener("click", function () {
-        liste.remove();
-      });
-      let updateButton = document.createElement("button");
-      updateButton.innerText = "Modifier";
-      updateButton.addEventListener("click", function () {});
-      liste.appendChild(updateButton);
-      liste.appendChild(suppr);
-      soft.appendChild(liste);
-      LSsoft.push(`
-        Produit : ${produit.value}
-        Stock : ${stock.value}
-        Achat HT : ${prixAchatHt}€
-        Vente HT : ${prixVenteHt}€
-        Marge HT : ${margeHt}€
-        TTC : ${prixTtc}€`);
+
+      const softData = {
+        produit: produit.value,
+        stock: stock.value,
+        prixAchatHt: prixAchatHt,
+        prixVenteHt: prixVenteHt,
+        margeHt: margeHt,
+        prixTtc: prixTtc,
+      };
+
+      LSsoft.push(softData);
       let LSsoftString = JSON.stringify(LSsoft);
       localStorage.setItem("LSsoft", LSsoftString);
+
+      renderStock([softData], soft);
     } else {
-      taxe = 0.2;
+      let taxe = 0.2;
       let prixAchatHt = achat.value;
       let prixVenteHt = vente.value;
       let margeHt = parseFloat(prixVenteHt) - parseFloat(prixAchatHt);
       let prixTtc = parseFloat(
         parseFloat(prixVenteHt) + parseFloat(prixVenteHt) * parseFloat(taxe)
       ).toFixed(2);
-      let liste = document.createElement("div");
-      liste.classList.add("liste");
-      liste.innerHTML = `<p class="nomProduit">${produit.value} <input class="modifyStock" type="number" value="${stock.value}"></input></p>
-            <select class="details">
-                <option>TTC : ${prixTtc}€</option>
-                <option>Achat HT : ${prixAchatHt}€</option>
-                <option>Vente HT : ${prixVenteHt}€</option>
-                <option>Marge HT : ${margeHt}€</option>
-                <option>Degré d'alcool : ${degres.value}%</option>
-            </select>`;
-      let suppr = document.createElement("button");
-      suppr.innerText = "X";
-      suppr.style.color = "#A52A2A";
-      suppr.addEventListener("click", function () {
-        liste.remove();
-      });
-      let updateButton = document.createElement("button");
-      updateButton.innerText = "Modifier";
-      updateButton.addEventListener("click", function () {});
-      liste.appendChild(updateButton);
-      liste.appendChild(suppr);
-      alcool.appendChild(liste);
 
-      LSalcool.push(`
-        Produit : ${produit.value}
-        Stock : ${stock.value}
-        Prix d'achat HT : ${prixAchatHt}€
-        Prix de vente HT : ${prixVenteHt}€
-        Marge HT : ${margeHt}€
-        TTC : ${prixTtc}€
-        Degré d'alcool : ${degres}%`);
+      const alcoolData = {
+        produit: produit.value,
+        stock: stock.value,
+        prixAchatHt: prixAchatHt,
+        prixVenteHt: prixVenteHt,
+        margeHt: margeHt,
+        prixTtc: prixTtc,
+        degres: degres.value,
+      };
+
+      LSalcool.push(alcoolData);
+      let LSalcoolString = JSON.stringify(LSalcool);
+      localStorage.setItem("LSalcool", LSalcoolString);
+
+      renderStock([alcoolData], alcool);
     }
-    save();
+
     let nomProduits = document.querySelectorAll(".nomProduit");
     nomProduits.forEach(function (nomProduit) {
       if (stock.value <= 5) {
@@ -124,35 +93,38 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  function save() {
-    localStorage.setItem("LSsoft", JSON.stringify(LSsoft));
-    localStorage.setItem("LSalcool", JSON.stringify(LSalcool));
-  }
-
-  function renderStock(stockData) {
+  function renderStock(stockData, container) {
     stockData.forEach(function (item) {
-      if (type.value == "soft") {
-        let liste = document.createElement("div");
-        liste.innerHTML += `<div class="liste"><p class="nomProduit">${item.produit}<input class="modifyStock" type="number" value="${stock.value}"></p>
-        <select class="details">
-        <option>TTC : ${item.prixTtc}€</option>
-        <option>Achat HT : ${item.prixAchatHt}€</option>
-        <option>Vente HT : ${item.prixVenteHt}€</option>
-        <option>Marge HT : ${item.margeHt}€</option>
-        </select></div>`;
-      } else {
-        let liste = document.createElement("div");
-        liste.innerHTML += `<div class="liste"><p class="nomProduit">${
-          item.produit
-        } <input class="modifyStock" type="number" value="${stock.value}"></p>
-        <select class="details">
-        <option>TTC : ${item.prixTtc}€</option>
-        <option>Achat HT : ${item.prixAchatHt}€</option>
-        <option>Vente HT : ${item.prixVenteHt}€</option>
-        <option>Marge HT : ${item.margeHt}€</option>
-        <option>Degré d'alcool : ${item.degres || ""}%</option>
-        </select></div>`;
-      }
+      let liste = document.createElement("div");
+      liste.classList.add("liste");
+      liste.innerHTML = `<p class="nomProduit">${
+        item.produit
+      } <input class="modifyStock" type="number" value="${
+        item.stock
+      }"></input></p>
+            <select class="details">
+                <option>TTC : ${item.prixTtc}€</option>
+                <option>Achat HT : ${item.prixAchatHt}€</option>
+                <option>Vente HT : ${item.prixVenteHt}€</option>
+                <option>Marge HT : ${item.margeHt}€</option>
+                ${
+                  item.degres
+                    ? `<option>Degré d'alcool : ${item.degres}%</option>`
+                    : ""
+                }
+            </select>`;
+      let suppr = document.createElement("button");
+      suppr.innerText = "X";
+      suppr.style.color = "#A52A2A";
+      suppr.addEventListener("click", function () {
+        liste.remove();
+      });
+      let updateButton = document.createElement("button");
+      updateButton.innerText = "Modifier";
+      updateButton.addEventListener("click", function () {});
+      liste.appendChild(updateButton);
+      liste.appendChild(suppr);
+      container.appendChild(liste);
     });
   }
 });
